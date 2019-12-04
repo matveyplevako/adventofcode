584f2a6da7146a1d71726adcfc2faa1e0820a0b6
matveyplevako <matveyplevako@rambler.ru>
2019-12-04 08:31:25 +0300

    day4

---
+++ adventofcode2019/day4/first.py
@@ -0,0 +1,22 @@
+with open("input.txt") as inp:
+    start, end = map(int, inp.readline().split("-"))
+
+fit = 0
+
+for i in range(start, end + 1):
+    digits = str(i)
+    has_same = False
+    for digit in digits:
+        # all in non decreasing so the same digits will be together
+        if digits.count(digit) >= 2:
+            has_same = True
+            break
+
+    if has_same and sorted(digits) == list(digits):
+        fit += 1
+
+print(fit)
+
+
+with open("out1.txt", "w") as out:
+    out.write(str(fit) + '\n')
