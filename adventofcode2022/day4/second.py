c298b722727239e5f9e282127f46e91167f5da94
matveyplevako <matveyplevako@rambler.ru>
2023-11-17 14:45:49 +0300

    day 4

---
+++ adventofcode2022/day4/second.py
@@ -0,0 +1,13 @@
+def main():
+    score = 0
+    with open("input.txt") as inp:
+        for line in inp.readlines():
+            a, b = line.strip().split(',')
+            a, b = [list(map(int, x.split('-'))) for x in [a, b]]
+            if (a[0] <= b[0] <= a[1]) or (b[0] <= a[0] <= b[1]):
+                score += 1
+        print(score)
+
+
+if __name__ == '__main__':
+    main()
