ac0393c1bd04c2fe442be8b17b7bf839f98cedbc
matveyplevako <matveyplevako@rambler.ru>
2023-11-17 10:35:42 +0300

    day 24

---
+++ adventofcode2021/day24/second.py
@@ -0,0 +1,34 @@
+def rec_search(a, b, c, ind, num, stack):
+    if a[ind] == 1:
+        for i in range(1, 10):
+            res = rec_search(a, b, c, ind + 1, num + [i], stack + [(i, c[ind])])
+            if res:
+                return res
+    else:
+        w_old, c_old = stack.pop()
+        w_new = w_old + c_old + b[ind]
+        if not 0 < w_new <= 9:
+            return
+        if ind == len(a) - 1:
+            return num + [w_new]
+        res = rec_search(a, b, c, ind + 1, num + [w_new], stack.copy())
+        return res
+
+
+def main():
+    inst_list = []
+    with open("input.txt") as inp:
+        for line in inp.readlines():
+            inst_list.append(line.split())
+
+    a = [int(x[-1]) for x in inst_list[4::18]]
+    b = [int(x[-1]) for x in inst_list[5::18]]
+    c = [int(x[-1]) for x in inst_list[15::18]]
+    print(a)
+    print(b)
+    print(c)
+    print(''.join(map(str, rec_search(a, b, c, 0, [], []))))
+
+
+if __name__ == '__main__':
+    main()
